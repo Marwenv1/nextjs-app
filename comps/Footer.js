3a32ce3dac77030625129eec94cff436ba375770
Marwenv1 <marwen.tlili@esprit.tn>
2021-06-16 23:44:36 +0100

    Layout added

---
+++ comps/Footer.js
@@ -0,0 +1,8 @@
+const Footer = () => {
+    return(
+       <div>
+           Copyright 2021 Marwenv1
+       </div>
+    );
+}
+export default Footer;
---
+++ pages/maro/index.js
@@ -0,0 +1,8 @@
+const maro = () => {
+    return(
+        <div>
+            <h1>This is Maro</h1>
+        </div>
+    )
+}
+export default maro;
